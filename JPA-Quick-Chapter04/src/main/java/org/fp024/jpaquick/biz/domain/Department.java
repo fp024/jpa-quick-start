05f08e6eded8933c99ad32e0f99812849249dd5b
fp024 <aopenasus@naver.com>
2021-02-10 08:59:36 +0900

    ~ p249 3.2 양방향 매핑 관계 검색
    양방향 매핑 관계설정후, EAGER, LAZY 각각 실행태스트.

--- JPA-Quick-Chapter04/src/main/java/org/fp024/jpaquick/biz/domain/Department.java
+++ JPA-Quick-Chapter04/src/main/java/org/fp024/jpaquick/biz/domain/Department.java
@@ -6,10 +6,14 @@ import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -23,4 +27,7 @@ public class Department {
 
     @Column(length = 25, nullable = false)
     private String name;
+
+    @OneToMany(mappedBy = "dept", fetch = FetchType.EAGER)
+    private List<Employee> employeeList = new ArrayList<>();
 }
---
+++ JPA-Quick-Chapter04/src/test/java/org/fp024/jpaquick/biz/ManyToOneBothWayClientTest.java
@@ -0,0 +1,74 @@
+package org.fp024.jpaquick.biz;
+
+import lombok.extern.slf4j.Slf4j;
+import org.fp024.jpaquick.biz.domain.Department;
+import org.fp024.jpaquick.biz.domain.Employee;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+@Slf4j
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ManyToOneBothWayClientTest {
+    private static EntityManagerFactory emf;
+
+    @BeforeAll
+    static void beforeAll() {
+        emf = Persistence.createEntityManagerFactory("Chapter04");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        if (emf != null) {
+            emf.close();
+        }
+    }
+
+
+    @Order(1)
+    @Test
+    void dataInsert() {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        // 부서등록
+        Department department = new Department();
+        department.setName("개발부");
+        em.persist(department);
+
+        // 직원등록
+        Employee employee1 = new Employee();
+        employee1.setName("탈보가이");
+        employee1.setDept(department);
+        em.persist(employee1);
+
+        // 직원등록
+        Employee employee2 = new Employee();
+        employee2.setName("다크나이트");
+        employee2.setDept(department);
+        em.persist(employee2);
+        tx.commit();
+
+        em.close();
+    }
+
+    @Order(2)
+    @Test
+    void dataSelect() {
+        EntityManager em = emf.createEntityManager();
+        Department department = em.find(Department.class, 1L);
+
+        logger.info("검색된 부서 : {}", department.getName());
+        logger.info("부서에 소속된 직원 명단");
+        department.getEmployeeList().forEach(employee -> logger.info("{} ({})", employee.getName(), employee.getDept().getName()));
+    }
+}

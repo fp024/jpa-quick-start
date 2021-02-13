f6d179d47b5cf711564710bec2d299fdeba7d344
fp024 <aopenasus@naver.com>
2021-02-14 03:44:13 +0900

    ~ p376 2.1 조인이 동작하지 않는 상황
    전체 직원을 조회했을 때..
    1. 전체 직원 조회
    2. 현재 직원 데이터에 연결된 부서 2개 조회 (뭔가 내부적으로 서브쿼리 식으로 조회된듯.. 조인없이..)
    
    [기타 내용]
    @Builder를 써서, 책의 내용처럼 직원 도메인에 부서를 셋팅할 때. 그 부서에 직원을 추가해줄 수 없음.
    테스트 코드 상에서 매핑 해줘야함.
    
    @Builder를 쓰면서 기본 생성자를 같이 사용하려면, 전체 멤버 생성자가 필요함.
    * JPA환경에서 기본 생성자는 반드시 필요.. 조회할 때 사용되는 것 같다.
    * @Builder와  @NoArgsConstructor를 같이썻을 때.. @AllArgsConstructor를 추가하지 않으면 다음 오류가 발생한다.
    
    @Builder(toBuilder = true)
    ^
      required: no arguments
      found: Long,String,String,LocalDateTime,String,String,Double,Double,Department
      reason: actual and formal argument lists differ in length
    
    그리고 인텔리J에서 변수에 보라색 밑 줄은 .. 람다내에서 외부 변수 사용할 때 표시해주는 거라고함.

--- JPA-Quick-Chapter06/src/main/java/org/fp024/jpaquick/biz/domain/Department.java
+++ JPA-Quick-Chapter06/src/main/java/org/fp024/jpaquick/biz/domain/Department.java
@@ -7,43 +7,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
-@Builder(toBuilder = true)
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
 @Entity
-@Table(name = "s_emp")
-public class Employee {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@Table(name = "s_dept")
+public class Department {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long deptId;
 
     private String name;
 
-    private String mailId;
-
-    private LocalDateTime startDate;
-
-    private String title;
-
-    private String deptName;
-
-    private Double salary;
-
-    private Double commissionPct;
-
-    @ManyToOne
-    @JoinColumn(name = "dept_id")
-    private Department dept;
-
+    @OneToMany(mappedBy = "dept", cascade = CascadeType.PERSIST)
+    private final List<Employee> employeeList = new ArrayList<>();
 }
---
+++ JPA-Quick-Chapter06/src/test/java/org/fp024/jpaquick/biz/client/JQLBasicJoinClientTest.java
@@ -0,0 +1,100 @@
+package org.fp024.jpaquick.biz.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.fp024.jpaquick.biz.domain.Department;
+import org.fp024.jpaquick.biz.domain.Employee;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Slf4j
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class JQLBasicJoinClientTest {
+    private static EntityManagerFactory emf;
+    private EntityManager em;
+
+    @BeforeAll
+    static void beforeAll() {
+        emf = Persistence.createEntityManagerFactory("Chapter06");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        emf.close();
+    }
+
+    @Order(1)
+    @Test
+    void dataInsert() {
+        em.getTransaction().begin();
+
+        int t = 3;
+
+        Department department1 = Department.builder().name("개발부").build();
+
+        IntStream.rangeClosed(1, 3).forEach(i -> {
+            Employee employee = Employee.builder()
+                    .name("개발직원" + i)
+                    .salary(i * 12700.00)
+                    .mailId("Dev-" + i)
+                    .dept(department1).build();
+            department1.getEmployeeList().add(employee);
+        });
+        em.persist(department1);
+
+
+        Department department2 = Department.builder().name("영업부").build();
+
+        IntStream.rangeClosed(1, 3).forEach(i -> {
+            Employee employee = Employee.builder()
+                    .name("영업직원" + i)
+                    .salary(i * 27300.00)
+                    .mailId("Sale-" + i)
+                    .dept(department2).build();
+            department2.getEmployeeList().add(employee);
+        });
+        em.persist(department2);
+
+        Department department3 = Department.builder().name("인재개발부").build();
+        em.persist(department3);
+
+        em.getTransaction().commit();
+    }
+
+    @Order(2)
+    @Test
+    void dataSelect() {
+        String jpql = "SELECT e FROM Employee e";
+
+        TypedQuery<Employee> query = em.createQuery(jpql, Employee.class);
+
+        List<Employee> result = query.getResultList();
+
+        logger.info("검색된 직원 목록");
+        result.forEach(employee -> logger.info(employee.getName()));
+
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        em = emf.createEntityManager();
+    }
+
+    @AfterEach
+    void afterEach() {
+        em.close();
+    }
+
+}

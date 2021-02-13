4ea9d3c682d0a9b47b276a25fc98c39d766bfe15
fp024 <aopenasus@naver.com>
2021-02-14 02:03:04 +0900

    ~ p357 1.5 파라미터 바인딩 - 위치 기반
    로깅관련 내용도 있는데, 이미 log4j2-slp4j 기반으로 설정하고 있어서 넘어가자..

--- JPA-Quick-Chapter06/src/test/java/org/fp024/jpaquick/biz/client/JQLBasicClientTest.java
+++ JPA-Quick-Chapter06/src/test/java/org/fp024/jpaquick/biz/client/JQLBasicClientTest.java
@@ -2,7 +2,6 @@ package org.fp024.jpaquick.biz.client;
 
 import lombok.extern.slf4j.Slf4j;
 import org.fp024.jpaquick.biz.domain.Employee;
-import org.fp024.jpaquick.biz.domain.EmployeeSalaryData;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -15,9 +14,9 @@ import org.junit.jupiter.api.TestMethodOrder;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
+import javax.persistence.Query;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 @Slf4j
@@ -64,17 +63,20 @@ public class JQLBasicClientTest {
     @Test
     void dataSelect() {
         // JPQL
-        String jpql = "SELECT " +
-                "NEW org.fp024.jpaquick.biz.domain.EmployeeSalaryData(id, salary, commissionPct) " +
-                "FROM Employee";
-
+        String jpql = "SELECT id, name, deptName, salary " +
+                "FROM Employee " +
+                "WHERE id=?1 AND name = ?2 ";
+        
         // JPQL 전송
-        TypedQuery<EmployeeSalaryData> query = em.createQuery(jpql, EmployeeSalaryData.class);
-        List<EmployeeSalaryData> resultList = query.getResultList();
+        Query query = em.createQuery(jpql);
+        query.setParameter(1, 1L);
+        query.setParameter(2, "직원1");
+
 
         // 검색 결과 처리
-        logger.info("검색된 직원 목록");
-        resultList.forEach(result -> logger.info("--> {}", result));
+        Object[] result = (Object[]) query.getSingleResult();
+        logger.info("{}번 직원의 정보", result[0]);
+        logger.info(Arrays.toString(result));
 
     }
 

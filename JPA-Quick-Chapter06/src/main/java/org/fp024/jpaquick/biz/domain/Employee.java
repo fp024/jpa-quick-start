57b28886bbf76b9d75a6d795a1e1567575f59a28
fp024 <aopenasus@naver.com>
2021-02-14 01:14:45 +0900

    ~ p353 1.2 JPQL 기초
    @Setter를 사용하지 않고, @Builder, @NoArgsConstructor, @AllArgsConstructor 조합으로 사용
    한번 객체를 생성했으면 내용을 바꾸지 않도록 하고, 필요할 경우 toBuilder를 true로 설정해서
    복사본에 필요한 변경값을 세팅하여 사용되도록 수정.
    
    https://www.baeldung.com/lombok-builder-custom-setter
    위에 나온데로 빌더의 custem setter를 설정해서 사용해보기도함.
    
    Setter를 재지정할 일이 있을 때는 toBuilder 기능을 활용해보는게 좋을 것 같다.

--- JPA-Quick-Chapter06/src/main/java/org/fp024/jpaquick/biz/domain/Employee.java
+++ JPA-Quick-Chapter06/src/main/java/org/fp024/jpaquick/biz/domain/Employee.java
@@ -1,20 +1,23 @@
 package org.fp024.jpaquick.biz.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 @Getter
-@Setter
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 @ToString
 @EqualsAndHashCode
 @Entity
@@ -24,7 +27,31 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 25, nullable = false)
     private String name;
 
+    private String mailId;
+
+    private LocalDateTime startDate;
+
+    private String title;
+
+    private String deptName;
+
+    private Double salary;
+
+    private Double commissionPct;
+
+    /**
+     * 빌더 정의 테스트
+     * 특정 필드에 대해서만 사용자 정의 동작을 넣어줄 수 있음.
+     * https://www.baeldung.com/lombok-builder-custom-setter
+     */
+    public static class EmployeeBuilder {
+        private String name;
+
+        public EmployeeBuilder name(String name) {
+            this.name = name + ":custom builder test";
+            return this;
+        }
+    }
 }
--- JPA-Quick-Chapter06/src/test/java/org/fp024/jpaquick/biz/client/JQLBasicClientTest.java
+++ JPA-Quick-Chapter06/src/test/java/org/fp024/jpaquick/biz/client/JQLBasicClientTest.java
@@ -1,19 +1,29 @@
 package org.fp024.jpaquick.biz.client;
 
 import lombok.extern.slf4j.Slf4j;
+import org.fp024.jpaquick.biz.domain.Employee;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.IntStream;
 
 @Slf4j
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class JQLBasicClientTest {
     private static EntityManagerFactory emf;
+    private EntityManager em;
 
     @BeforeAll
     static void beforeAll() {
@@ -22,15 +32,61 @@ public class JQLBasicClientTest {
 
     @AfterAll
     static void afterAll() {
-        if (emf != null) {
-            emf.close();
-        }
+        emf.close();
     }
 
-
+    @Order(1)
     @Test
-    void test() {
+    void dataInsert() {
+        em.getTransaction().begin();
 
+        IntStream.rangeClosed(1, 10).forEach(
+                i -> {
+                    Employee employee = Employee.builder()
+                            .name("직원" + i)
+                            .mailId("anti-corona" + i)
+                            .deptName("개발부")
+                            .salary(12700.00 * i)
+                            .startDate(LocalDateTime.now())
+                            .title("사원")
+                            .commissionPct(15.00)
+                            .build();
+
+                    // toBuilder 테스트
+                    Employee.EmployeeBuilder clonedBuilder = employee.toBuilder();
+                    clonedBuilder.name(employee.getName() + "+" + i);
+
+                    em.persist(clonedBuilder.build());
+                }
+        );
+
+        em.getTransaction().commit();
+    }
+
+    @Order(2)
+    @Test
+    void dataSelect() {
+        // JPQL
+        String jpql = "SELECT e FROM Employee AS e";
+
+        // JPQL 전송
+        TypedQuery<Employee> query = em.createQuery(jpql, Employee.class);
+        List<Employee> resultList = query.getResultList();
+
+        // 검색 결과 처리
+        logger.info("검색된 직원 목록");
+        resultList.forEach(employee -> logger.info("-->{}", employee));
+
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        em = emf.createEntityManager();
+    }
+
+    @AfterEach
+    void afterEach() {
+        em.close();
     }
 
 }

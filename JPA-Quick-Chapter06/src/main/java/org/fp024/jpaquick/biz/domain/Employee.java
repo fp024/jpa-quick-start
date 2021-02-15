058f1ff40d865dd892ae44a859e60cce4b192dbf
fp024 <aopenasus@naver.com>
2021-02-16 05:54:15 +0900

    ~ p418 4.1 이름이 부여된 쿼리 사용하기

--- JPA-Quick-Chapter06/src/main/java/org/fp024/jpaquick/biz/domain/Employee.java
+++ JPA-Quick-Chapter06/src/main/java/org/fp024/jpaquick/biz/domain/Employee.java
@@ -7,13 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
@@ -23,6 +17,13 @@ import java.time.LocalDateTime;
 @ToString
 @Entity
 @Table(name = "s_emp")
+@NamedQueries({
+        @NamedQuery(name = "Employee.searchById",
+                query = "SELECT e FROM Employee e WHERE e.id = :searchKeyword"),
+        @NamedQuery(name = "Employee.searchByName",
+                query = "SELECT e FROM Employee e WHERE e.name LIKE :searchName")
+
+})
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ JPA-Quick-Chapter06/src/test/java/org/fp024/jpaquick/biz/client/JQLNamedQueryClientTest.java
@@ -0,0 +1,102 @@
+package org.fp024.jpaquick.biz.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.fp024.jpaquick.biz.domain.Department;
+import org.fp024.jpaquick.biz.domain.Employee;
+import org.junit.jupiter.api.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Slf4j
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class JQLNamedQueryClientTest {
+    private static EntityManagerFactory emf;
+    private EntityManager em;
+
+    @BeforeAll
+    static void beforeAll() {
+        emf = Persistence.createEntityManagerFactory("Chapter06");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        emf.close();
+    }
+
+    @Order(1)
+    @Test
+    void dataInsert() {
+        em.getTransaction().begin();
+        Department department1 = Department.builder().name("개발부").build();
+
+        IntStream.rangeClosed(1, 3).forEach(i -> {
+            Employee employee = Employee.builder()
+                    .name("개발직원" + i)
+                    .salary(i * 12700.00)
+                    .mailId("Dev-" + i)
+                    .dept(department1).build();
+            department1.getEmployeeList().add(employee);
+        });
+        em.persist(department1);
+
+
+        Department department2 = Department.builder().name("영업부").build();
+
+        IntStream.rangeClosed(1, 3).forEach(i -> {
+            Employee employee = Employee.builder()
+                    .name("영업직원" + i)
+                    .salary(i * 27300.00)
+                    .mailId("Sale-" + i)
+                    .dept(department2).build();
+            department2.getEmployeeList().add(employee);
+        });
+        em.persist(department2);
+
+        Department department3 = Department.builder().name("인재개발부").build();
+        em.persist(department3);
+
+        // 부서 정보가 없는 새로운 직원추가
+        Employee employee = Employee.builder()
+                .name("아르바이트")
+                .mailId("Alba-01")
+                .salary(10000.00)
+                .build();
+        em.persist(employee);
+
+
+        // 이름이 영업부인 새로운 직원 추가
+        Employee employee2 = Employee.builder()
+                .name("영업부")
+                .build();
+        em.persist(employee2);
+
+        em.getTransaction().commit();
+    }
+
+    @Order(2)
+    @Test
+    void dataSelect() {
+
+        TypedQuery<Employee> query = em.createNamedQuery("Employee.searchByName", Employee.class);
+        query.setParameter("searchName", "%영업%");
+
+        List<Employee> result = query.getResultList();
+        result.forEach(row -> logger.info("===> {}번 직원의 이름: {}", row.getId(), row.getName()));
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        em = emf.createEntityManager();
+    }
+
+    @AfterEach
+    void afterEach() {
+        em.close();
+    }
+
+}

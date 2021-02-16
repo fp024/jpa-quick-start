1078d7374bef5e8df3f0faace3a24ce0b5853268
fp024 <aopenasus@naver.com>
2021-02-16 10:29:48 +0900

    ~ p443 JPQL만을 이용하여 검색

--- JPA-Quick-Chapter07/src/test/java/org/fp024/jpaquick/biz/client/CriteriaSearchClientTest.java
+++ JPA-Quick-Chapter07/src/test/java/org/fp024/jpaquick/biz/client/CriteriaSearchClientTest.java
@@ -1,11 +1,16 @@
 package org.fp024.jpaquick.biz.client;
 
 import lombok.extern.slf4j.Slf4j;
+import org.fp024.jpaquick.biz.domain.Employee;
 import org.junit.jupiter.api.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.IntStream;
 
 @Slf4j
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -26,12 +31,70 @@ class CriteriaSearchClientTest {
     @Order(1)
     @Test
     void dataInsert() {
+        em.getTransaction().begin();
+        // 직원 정보 등록
+        IntStream.rangeClosed(1, 3).forEach(i -> {
+            Employee employee = Employee.builder()
+                    .name("개발맨 " + i)
+                    .mailId("Corona " + i)
+                    .deptName("개발부")
+                    .salary(12700.00 * i)
+                    .startDate(LocalDateTime.now())
+                    .commissionPct(10.00)
+                    .build();
+            em.persist(employee);
+        });
 
+        IntStream.rangeClosed(1, 3).forEach(i -> {
+            Employee employee = Employee.builder()
+                    .name("영업맨 " + i)
+                    .mailId("Virus " + i)
+                    .deptName("영업부")
+                    .salary(23800.00 * i)
+                    .startDate(LocalDateTime.now())
+                    .title("과장")
+                    .commissionPct(15.00)
+                    .build();
+            em.persist(employee);
+        });
+
+        // 부서 정보가 없는 직원 등록
+        Employee employee = Employee.builder()
+                .name("아르바이트")
+                .mailId("Alba-01")
+                .salary(10000.00)
+                .build();
+        em.persist(employee);
+
+        em.getTransaction().commit();
     }
 
     @Order(2)
     @Test
     void dataSelect() {
+        // 검색 정보 설정
+        String searchCondition = "NAME";
+        String searchKeyword = "아르바이트";
+
+        // 검색관련 쿼리
+        String jpqlByMailId = "SELECT e FROM Employee e WHERE e.mailId = :searchKeyword";
+
+        String jpqlByName = "SELECT e FROM Employee e WHERE e.name = :searchKeyword";
+
+        TypedQuery<Employee> query;
+
+        // 검색조건에 따른 분기 처리
+        if (searchCondition.equals("NAME")) {
+            query = em.createQuery(jpqlByName, Employee.class);
+        } else {
+            query = em.createQuery(jpqlByMailId, Employee.class);
+        }
+
+        query.setParameter("searchKeyword", searchKeyword);
+
+        List<Employee> resultList = query.getResultList();
+
+        resultList.forEach(e -> logger.info("---> {}", e));
 
     }
 

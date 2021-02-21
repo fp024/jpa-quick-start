b7674d1700f9b8333a663aeabaa81cde92457c75
fp024 <aopenasus@naver.com>
2021-02-21 10:38:56 +0900

    ~ p652 프리젠테이션 레이어 개발
    이부분의 설정도 대부분 Gradle - Spring MVC 프로젝트 만들때 거의 다 한내용이고,
    IntelliJ에서 out폴더에 war압축이 풀리도록 하고 그것으로 보도록 했다.
    
    Build Artifacts.. 에 단축키 없어서 단축키 지정함 Ctrl + \

---
+++ ruby_shopping/src/main/java/org/fp024/jpaquick/shopping/web/controller/IndexController.java
@@ -0,0 +1,14 @@
+package org.fp024.jpaquick.shopping.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String main() {
+        return "index";
+    }
+
+}
---
+++ ruby_shopping/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html lang="ko">
+<head>
+    <title>Ruby Shopping Town</title>
+</head>
+<style>
+    h1 {text-align: center;}
+</style>
+<body>
+    <h1>루비 쇼핑몰을 이용해주셔서 감사합니다.</h1>
+    <br><br><br><br>
+</body>
+</html>
